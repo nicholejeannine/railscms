1eb96ebcc26d5801e7162f81aa0b7040d407d815
nicholejeannine <nklerk@alopias.com>
2017-03-24 19:05:09 -0700

    Pull images from controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 class MainController < ApplicationController
+  before_action :get_images
   def index
+  end
+  
+  private
+  def get_images
+    @images = %w(https://lorempixel.com/1500/500/nature/1 https://lorempixel.com/1500/500/nature/2 https://lorempixel.com/1500/500/nature/3)
   end
 end
