8853ae388786857d6a3983384b1d93d3f8ef05ff
nicholejeannine <nklerk@alopias.com>
2017-03-29 12:34:26 -0700

    Disallow null urls  on photos and add sort order column for photos

--- db/migrate/20170328181820_create_photos.rb
+++ db/migrate/20170328181820_create_photos.rb
@@ -1,8 +1,9 @@
 class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
     create_table :photos do |t|
-      t.string :url
+      t.string :url, null: false
       t.string :caption
+      t.integer :sort_order, null: false
       t.references :album, foreign_key: true
 
       t.timestamps
