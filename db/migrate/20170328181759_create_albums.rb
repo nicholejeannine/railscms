04220995f252c31861ba09712b7aed306f7ffbad
nicholejeannine <nklerk@alopias.com>
2017-03-29 12:33:35 -0700

    Disallow null titles on albums

--- db/migrate/20170328181759_create_albums.rb
+++ db/migrate/20170328181759_create_albums.rb
@@ -1,7 +1,7 @@
 class CreateAlbums < ActiveRecord::Migration[5.0]
   def change
     create_table :albums do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
       t.string :cover
       t.references :user, foreign_key: true
