40e6324a87fc35f82af352cad142878e41afb996
nicholejeannine <nklerk@alopias.com>
2017-04-04 16:45:17 -0700

    Modify routes and navigation path links

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resource :photos, only: [:index, :new, :create]
 
     get 'photos' => "photos#index"
+    get 'cms' => "pages#index", as: 'cms_root'
   end
   root to: 'public#index'
 end
