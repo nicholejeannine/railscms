af9b8c43a97c5f111984b716469117920a93caee
nicholejeannine <nklerk@alopias.com>
2017-04-04 19:06:50 -0700

    Order photos

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -5,7 +5,7 @@ class PublicController < ApplicationController
   
   private
   def get_images
-    @photos = Photo.all
+    @photos = Photo.order(:sort_order)
     # @images = %w(https://lorempixel.com/1500/500/nature/1 https://lorempixel.com/1500/500/nature/2 https://lorempixel.com/1500/500/nature/3)
   end
 end
